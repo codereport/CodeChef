42cccfe74aafbf22cdb95b31c1e0d699e737fa61
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-04-22 16:04:47 -0400

    Create CookOff_2018_04_P2.cpp

---
+++ CookOff_2018_04_P2.cpp
@@ -0,0 +1,39 @@
+// code_reoport Solution
+// 
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+string binary_nim (const vector<string>& stacks, const string& first) 
+{
+   int a = 0, b = 0;
+   for (const auto& stack : stacks) 
+   {
+      if (stack[0] == '0') a += count (stack.begin (), stack.end (), '0');
+      if (stack[0] == '1') b += count (stack.begin (), stack.end (), '1');
+   }
+
+   if (a == b) return (first == "Dee" ? "Dum" : "Dee");
+   else        return (a > b          ? "Dee" : "Dum");
+}
+
+int main ()
+{
+   int t; cin >> t;
+
+   while (t--) 
+   {
+      int n; string s; 
+      cin >> n >> s;
+      vector<string> v (n);
+      for (int i = 0; i < n; i++) cin >> v[i];
+
+      cout << binary_nim (v, s) << endl;
+   }
+
+   return 0;
+}
