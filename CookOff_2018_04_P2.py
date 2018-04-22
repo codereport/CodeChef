ce561b4a1adb6cee84fe393c14d1d231200b0f85
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-04-22 17:33:46 -0400

    Create CookOff_2018_04_P2.py

---
+++ CookOff_2018_04_P2.py
@@ -0,0 +1,28 @@
+// code_report Solution
+// 
+
+
+def binary_nim (stacks, first):
+
+   a, b = 0, 0
+   for stack in stacks:
+      if stack[0] == '0': a += stack.count ('0')
+      if stack[0] == '1': b += stack.count ('1')
+
+   if a == b: return ("Dum" if first == "Dee" else "Dee")
+   else:      return ("Dee" if a > b          else "Dum")
+
+# Code Execution Start
+
+t = int(input ())
+
+for i in range (t):
+   n = input ()
+   n, s = n.split ()
+   n = int (n)
+   v = []
+   for j in range (n): 
+      x = input ()
+      v.append (x)
+
+   print (binary_nim (v, s))
