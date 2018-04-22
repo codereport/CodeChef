0f5db8f3477d3bd0a5d105f2bb85f53788975fe3
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-04-22 16:53:49 -0400

    Create CookOff_2018_04_P2.java

---
+++ CookOff_2018_04_P2.java
@@ -0,0 +1,39 @@
+// code_report Solution
+// 
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    public static String binary_nim (String[] stacks, String first) 
+    {
+       int a = 0, b = 0;
+       for (String stack : stacks) 
+       {
+          if (stack.charAt (0) == '0') a += stack.chars ().filter (ch -> ch == '0').count ();
+          if (stack.charAt (0) == '1') b += stack.chars ().filter (ch -> ch == '1').count ();
+       }
+    
+       if (a == b) return (first.equals ("Dee") ? "Dum" : "Dee");
+       else        return (a > b                ? "Dee" : "Dum");
+    }
+    
+     public static void main(String []args)
+     {
+        Scanner scan = new Scanner (System.in);
+        int t = scan.nextInt ();
+        
+        for (int j = 0; j < t; j++)
+        {
+            int    n = scan.nextInt ();
+            String s = scan.next ();
+            
+            String[] v = new String[n];
+            
+            for (int i = 0; i < n; i++) v[i] = scan.next ();
+            
+            System.out.println (binary_nim (v, s));
+        }
+     }
+}
