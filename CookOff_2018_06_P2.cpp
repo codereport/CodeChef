026689906dd64b1ba37df387382d0dc6be991e01
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-06-19 08:37:51 -0400

    Update CookOff_2018_06_P2.cpp

--- CookOff_2018_06_P2.cpp
+++ CookOff_2018_06_P2.cpp
@@ -1,5 +1,3 @@
-// code_report
-// 
 #include <vector>
 #include <algorithm>
 #include <iostream>
@@ -22,25 +20,19 @@ int main () {
    int t, n, k; cin >> t;
    while (t--) 
    {
-      cin >> n >> k;                   // a - indices with no numbers
-      vector<int> a, b (n), c;         // b - unfinished permutation
-                                       // c - missing numbers
-      for (int i = 0; i < n; ++i) {
-         cin >> b[i];
-         if (b[i] == 0) a.push_back (i);
-      }
-      
-      for (int i = 1; i <= n; ++i) {
-         if (find (b.begin (), b.end (), i) == b.end ()) c.push_back (i);
-      }
+      cin >> n >> k;
+      vector<int> a (n), b; // a - unfinished permutation, b - missing numbers
+
+      for (int i = 0; i  < n; ++i) cin >> a[i];
+      for (int i = 1; i <= n; ++i) if (find (a.begin (), a.end (), i) == a.end ()) b.push_back (i);
 
       int ans = 0;
 
       do {
-         auto d = b; // finished permutation
-         for (int i = 0; i < a.size (); ++i) d[a[i]] = c[i];
+         auto d = a; // finished permutation
+         for (int i = 0, j = 0; i < n; ++i) if (d[i] == 0) d[i] = b[j], j++;
          if (adjacent_count_greater_than (d.begin (), d.end ()) == k) ans++;
-      } while (next_permutation (c.begin (), c.end ()));
+      } while (next_permutation (b.begin (), b.end ()));
       
       cout << ans << endl;
    }
