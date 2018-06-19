ef3815403111be1baabcd359ca6d6302e182eeae
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-06-19 08:25:49 -0400

    Update CookOff_2018_06_P2.cpp

--- CookOff_2018_06_P2.cpp
+++ CookOff_2018_06_P2.cpp
@@ -1,6 +1,5 @@
 // code_report
 // 
-
 #include <vector>
 #include <algorithm>
 #include <iostream>
@@ -8,18 +7,31 @@
 
 using namespace std;
 
+template<typename InIt>
+auto adjacent_count_greater_than (InIt first, InIt last) {
+   auto num = 0;
+   auto it = first;
+   while (true) {
+      it = adjacent_find (it, last, [](auto a, auto b) { return b > a; });
+      if (it == last) return num;
+      num++, it++;
+   }
+}
+
 int main () {
    int t, n, k; cin >> t;
    while (t--) 
    {
       cin >> n >> k;                   // a - indices with no numbers
-      vector<int> a, b (n), c (n);     // b - unfinished permutation
-      iota (c.begin (), c.end (), 1);  // c - missing numbers
-
+      vector<int> a, b (n), c;         // b - unfinished permutation
+                                       // c - missing numbers
       for (int i = 0; i < n; ++i) {
          cin >> b[i];
          if (b[i] == 0) a.push_back (i);
-         else c.erase (find (c.begin (), c.end (), b[i]));
+      }
+      
+      for (int i = 1; i <= n; ++i) {
+         if (find (b.begin (), b.end (), i) == b.end ()) c.push_back (i);
       }
 
       int ans = 0;
@@ -27,12 +39,11 @@ int main () {
       do {
          auto d = b; // finished permutation
          for (int i = 0; i < a.size (); ++i) d[a[i]] = c[i];
-         int j = 0;
-         for (int i = 0; i < n - 1; ++i) if (d[i + 1] > d[i]) j++;
-         if (j == k) ans++;
+         if (adjacent_count_greater_than (d.begin (), d.end ()) == k) ans++;
       } while (next_permutation (c.begin (), c.end ()));
       
       cout << ans << endl;
    }
+
    return 0;
 }
