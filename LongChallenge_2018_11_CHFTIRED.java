978f428e05ec3f9775ec8c9fd76c19fef70697c3
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-11-12 01:30:49 -0800

    Create LongChallenge_2018_11_CHFTIRED.java

---
+++ LongChallenge_2018_11_CHFTIRED.java
@@ -0,0 +1,12 @@
+// code_report Solution
+// https://youtu.be/ItUsACYn5yM
+
+import java.util.Scanner;
+
+public class Main {
+     public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- != 0) System.out.println("YES");
+     }
+}
