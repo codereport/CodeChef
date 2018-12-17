4134e45bcd7fdb2ae1ab5120038245d96fd6d888
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-12-17 11:31:50 -0800

    Create LongChallenge_2018_12_CHFIDEAL.cpp

---
+++ LongChallenge_2018_12_CHFIDEAL.cpp
@@ -0,0 +1,12 @@
+// code_report Solution
+// https://youtu.be/Og_68xJODPc
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   cout << 1 << endl;
+   int x; cin >> x;
+   cout << (x == 2 ? 3 : 2) << endl;
+   return 0;
+}
