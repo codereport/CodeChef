70b2e501630d665b1515903a210d7b86f6c48465
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-12-17 11:31:55 -0800

    Create LongChallenge_2018_12_CHFIDEAL.java

---
+++ LongChallenge_2018_12_CHFIDEAL.java
@@ -0,0 +1,14 @@
+// code_report Solution
+// https://youtu.be/Og_68xJODPc
+
+import java.util.Scanner;
+
+public class Main {
+     public static void main(String []args){
+        System.out.println(1);
+        System.out.flush();
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(x == 2 ? 3 : 2);
+     }
+}
