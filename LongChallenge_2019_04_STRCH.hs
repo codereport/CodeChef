e4e3b00cb15dae9f9b786d32915053f9e4f5dc73
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2019-04-18 19:30:35 -0700

    Create LongChallenge_2019_04_STRCH.hs

---
+++ LongChallenge_2019_04_STRCH.hs
@@ -0,0 +1,27 @@
+-- code_report Solution
+-- Video Link: 
+-- Problem Link: https://www.codechef.com/APRIL19B/problems/STRCH/
+
+import Control.Monad
+
+-- from Data.List.HT (utility-ht)
+chop :: (a -> Bool) -> [a] -> [[a]]
+chop p =
+   uncurry (:) .
+   foldr (\ x ~(y,ys) -> if p x then ([],y:ys) else ((x:y),ys) ) ([],[])
+
+second xs = map fst $ filter (even . snd) $ zip xs [1..]
+format = map (\a -> (init (init a), last a))
+
+sumN :: Int -> Int
+sumN n = div (n*(n+1)) 2
+
+solve :: (String, Char) -> Int
+solve (xs, c) = sumN (length xs) - (sum $ map (sumN . length) ys)
+    where ys = chop (==c) xs
+
+main :: IO ()
+main = do
+    t <- readLn :: IO Int
+    vs <- replicateM (2 * t) $ getLine
+    mapM_ print $ map solve $ format $ second vs
